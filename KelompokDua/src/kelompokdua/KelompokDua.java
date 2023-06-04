9b27746fad334f3735197f9c00a667d7a0ff17fe
bagasseptyono <muhammadbagasmbs@gmail.com>
2023-06-04 14:32:09 +0700

    tambah tampilan penjualan

---
+++ KelompokDua/src/kelompokdua/KelompokDua.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kelompokdua;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class KelompokDua {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                Penjualan penjualan = new Penjualan();
+                penjualan.setVisible(true);
+            }
+        });
+    }
+    
+}
